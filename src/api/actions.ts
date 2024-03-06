fcc82f4b6f5ea0ed130e7a7e4d76e55cc63469df
Reena Popat <63417789+Reena216@users.noreply.github.com>
2024-03-06 23:46:18 +0000

    updated

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://localhost:3000/api";
+const API_URL = "https://ominous-tribble-vrxj7756pp53pr5j-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
